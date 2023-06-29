a16418a7cc1abb2eb47a1dd9c34e23b52e5b3fc1
Ray Bilodeau <rbil@DESKTOP-B5KI19D>
2023-06-29 18:29:46 -0400

    added functionality to display puppy info when name is clicked.'
    clear
    q
    qfgnlvsvn
    
    ffsv
    git status

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { puppyList } from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
-  console.log(puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
-      <div>
+      <div className='App'>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
           })
+
         }
+
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+
       </div>
 
     </>
